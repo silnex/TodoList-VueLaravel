5fdbd6988a97ca3d6b67bba4f1515899f53574c2
silnex <silnex@silnex.kr>
2019-05-16 00:15:31 +0900

    Add check column

--- app/Todo.php
+++ app/Todo.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Todo extends Model
 {
-    //
+    protected $fillable = ['check', 'title', 'description', 'user_id'];
+
+    protected $casts = [
+        'check' => 'boolean'
+    ];
 }
